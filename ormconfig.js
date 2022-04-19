bd1b65ad8af0d7c12e800ed7c370be21688ceeda
Giovanni Perrotta <gioperrotta@gmail.com>
2022-04-19 09:03:48 -0300

    typeormprjbase version 1.01 create ormconfig.js

---
+++ ormconfig.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "type": "postgres",
+  "url":process.env.DATABASE_URL,
+  "entities": [
+    "dist/database/models/**/*.js"
+ ],
+ "migrations": [
+  "dist/database/migrations/**/*.js"
+],
+ "cli":{
+  "migrationsDir": [
+    "src/database/migrations/"
+  ],
+  "entitiesDir": "src/database/models"
+  }
+}
